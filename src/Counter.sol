2e74954ce86506ed58451d29073606aea8840391
Papais <8097453+LeoPapais@users.noreply.github.com>
2024-04-04 19:16:43 -0300

    first commit

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,20 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
-contract Counter {
-    uint256 public number;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+contract ProofOfWork is ERC20, Ownable, ERC20Permit, ERC20FlashMint {
+    constructor(address initialOwner)
+        ERC20("ProofOfWork", "PoW")
+        Ownable(initialOwner)
+        ERC20Permit("ProofOfWork")
+    {}
 
-    function increment() public {
-        number++;
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
